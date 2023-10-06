0913d874f1028a3e813ae438969c3e426bf31850
krisna <krisnamadani@gmail.com>
2023-10-06 21:54:17 +0700

    change navbar width

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             Krisna Madani
